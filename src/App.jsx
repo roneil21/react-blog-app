9e451ff5a55eb2b3caa726a6d4eace29ff53105f
Roneil <roneilalgar@gmail.com>
2023-08-11 13:24:39 +0800

    add note

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,28 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CreateBlog from "./components/CreateBlog";
 import BlogContent from "./components/BlogContent";
 
-
 const App = () => {
   return (
-    <>
-      {/* //I'm Using React Router v5 */}
-      <Router>
-        <div className="App">
-          <Navbar />
-          <div className="content">
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route path="/create">
-                <CreateBlog />
-              </Route>
-              <Route path="/blogs/:id">
-                <BlogContent />
-              </Route>
-            </Switch>
-          </div>
+
+    //I'm Using React Router v5
+    <Router>  
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <CreateBlog />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogContent />
+            </Route>
+          </Switch>
         </div>
-      </Router>
-      
-    </>
+      </div>
+    </Router>
   );
 };
 
